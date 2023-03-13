6c6e960577fce32284afefd28be1e6f1fc75c557
«Viktoria» <vika.mel.2015@mail.ru>
2023-03-13 10:40:59 +0300

    programma

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Сложение двух чисел
+int a,b =0;
+Console.Write("введите число a = ");
+a = int.Parse(Console.ReadLine());
+Console.Write("введите число b = ");
+b = int.Parse(Console.ReadLine());
+
+int t = 0;
+t=a+b;
+Console.WriteLine($"Итоговая сумма = {t}");
+
